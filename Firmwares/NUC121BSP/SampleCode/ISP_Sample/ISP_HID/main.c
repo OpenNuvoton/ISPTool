f5a24f4821081cdda4f6722ef424d13c03afe5e9
MS60 CCTu <CCTU@nuvoton.com>
2018-01-23 17:08:31 +0800

    Reduced code size of NUC121 ISP_HID sample

--- Firmwares/NUC121BSP/SampleCode/ISP_Sample/ISP_HID/main.c
+++ Firmwares/NUC121BSP/SampleCode/ISP_Sample/ISP_HID/main.c
@@ -21,16 +21,17 @@ void SYS_Init(void)
     /*---------------------------------------------------------------------------------------------------------*/
 
     /* Enable Internal HIRC 48 MHz clock */
-    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN);
+    CLK->PWRCTL |= CLK_PWRCTL_HIRCEN;
 
     /* Waiting for Internal RC clock ready */
-    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+    while (!(CLK->STATUS & CLK_STATUS_HIRCSTB_Msk));
 
     /* Switch HCLK clock source to Internal HIRC and HCLK source divide 1 */
-    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLKSEL_Msk)) | CLK_CLKSEL0_HCLKSEL_HIRC;
+    CLK->CLKDIV0 = (CLK->CLKDIV0 & (~CLK_CLKDIV0_HCLKDIV_Msk)) | CLK_CLKDIV0_HCLK(1);
 
     /* Enable module clock */
-    CLK_EnableModuleClock(USBD_MODULE);
+    CLK->APBCLK0 |= CLK_APBCLK0_USBDCKEN_Msk;
 
     /* Enable IP clock */
     CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL3_USBDSEL_HIRC, CLK_CLKDIV0_USB(1));
