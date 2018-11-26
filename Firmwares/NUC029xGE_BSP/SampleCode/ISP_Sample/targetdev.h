580dbad8bc7e0378bd1eeb3a9980cc22125a43ec
CCTu <CCTu@nuvoton.com>
2018-11-26 14:24:43 +0800

    Added NUC029xGE ISP_Sample

---
+++ Firmwares/NUC029xGE_BSP/SampleCode/ISP_Sample/targetdev.h
@@ -0,0 +1,12 @@
+
+#include "NUC029xGE.h"
+#include "isp_user.h"
+#include "ISP_HID\hid_transfer.h"
+#include "ISP_UART\uart_transfer.h"
+
+#define DetectPin   			PA3
+
+/* rename for uart_transfer.c */
+#define UART_T					UART0
+#define UART_T_IRQHandler		UART02_IRQHandler
+#define UART_T_IRQn				UART02_IRQn
