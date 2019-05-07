bf06689b72add9b30ea4cc2b6617a1d064ad59eb
CCTu <CCTu@nuvoton.com>
2019-05-07 15:14:56 +0800

    ISP_Samples for Audio Chip are out of date. User can find the latest ISP Sample in Audio Chip series BSP.

--- Firmwares/I91000_BSP/SampleCode/ISP_Sample/ISP_UART/targetdev.h
+++
@@ -1,9 +0,0 @@
-
-#include "ISD9000.h"
-#include "uart_transfer.h"
-#include "ISP_USER.h"
-
-/* rename for uart_transfer.c */
-#define UART_N							UART0
-#define UART_N_IRQHandler		UART0_IRQHandler
-#define UART_N_IRQn					URT0_IRQn
--- Firmwares/I91200_BSP/SampleCode/ISP_Sample/ISP_UART/targetdev.h
+++
@@ -1,9 +0,0 @@
-
-#include "I91200.h"
-#include "uart_transfer.h"
-#include "ISP_USER.h"
-
-/* rename for uart_transfer.c */
-#define UART_N							UART0
-#define UART_N_IRQHandler		UART0_IRQHandler
-#define UART_N_IRQn					UART0_IRQn
